0e9d1a9cf892ce12c6ab10c359de0713a9f1636c
Arcnilya <jonathan.magnusson@kau.se>
2023-01-30 13:46:00 +0100

    Plugin fix

--- __init__.py
+++ __init__.py
@@ -1,70 +1,6 @@
 from CTFd.plugins import register_plugin_assets_directory
-
-
-class FlagException(Exception):
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return self.message
-
-
-class BaseFlag(object):
-    name = None
-    templates = {}
-
-    @staticmethod
-    def compare(self, saved, provided):
-        return True
-
-class CTFdStaticFlag(BaseFlag):
-    name = "static"
-    templates = {  # Nunjucks templates used for key editing & viewing
-        "create": "/plugins/flags/assets/static/create.html",
-        "update": "/plugins/flags/assets/static/edit.html",
-    }
-
-    @staticmethod
-    def compare(chal_key_obj, provided):
-        saved = chal_key_obj.content
-        data = chal_key_obj.data
-
-        if len(saved) != len(provided):
-            return False
-        result = 0
-
-        if data == "case_insensitive":
-            for x, y in zip(saved.lower(), provided.lower()):
-                result |= ord(x) ^ ord(y)
-        else:
-            for x, y in zip(saved, provided):
-                result |= ord(x) ^ ord(y)
-        return result == 0
-
-
-class CTFdRegexFlag(BaseFlag):
-    name = "regex"
-    templates = {  # Nunjucks templates used for key editing & viewing
-        "create": "/plugins/flags/assets/regex/create.html",
-        "update": "/plugins/flags/assets/regex/edit.html",
-    }
-
-    @staticmethod
-    def compare(chal_key_obj, provided):
-        saved = chal_key_obj.content
-        data = chal_key_obj.data
-
-        try:
-            if data == "case_insensitive":
-                res = re.match(saved, provided, re.IGNORECASE)
-            else:
-                res = re.match(saved, provided)
-        # TODO: this needs plugin improvements. See #1425.
-        except re.error as e:
-            raise FlagException("Regex parse error occured") from e
-
-        return res and res.group() == provided
-
+from CTFd.plugins.flags import FlagException, BaseFlag, get_flag_class, FLAG_CLASSES
+from CTFd.models import db, Solves, WrongKeys, Keys, Challenges, Files, Tags, Teams, Hints
 
 class CTFdDVAD25Flag(BaseFlag):
     name = "DVAD25"
@@ -76,24 +12,15 @@ class CTFdDVAD25Flag(BaseFlag):
     @staticmethod
     def compare(chal_key_obj, provided):
         saved = chal_key_obj.content
-        world = chal_key_obj.world
-        machine = chal_key_obj.machine
+        #world = chal_key_obj.world
+        #machine = chal_key_obj.machine
 
-        if provided == "flag{"+f"{saved}{world}{machine}"+"}":
+        if provided == saved:
             return True
         else:
             return False
 
 
-FLAG_CLASSES = {"static": CTFdStaticFlag, "regex": CTFdRegexFlag, "DVAD25": CTFdDVAD25Flag}
-
-
-def get_flag_class(class_id):
-    cls = FLAG_CLASSES.get(class_id)
-    if cls is None:
-        raise KeyError
-    return cls
-
-
 def load(app):
-    register_plugin_assets_directory(app, base_path="/plugins/flags/assets/")
+    FLAG_CLASSES['DVAD25'] = CTFdDVAD25Flag
+    register_plugin_assets_directory(app, base_path="/plugins/DVAD25Flags/assets/")
